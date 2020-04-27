d16b4a948179728683e675dbfa956aff564c5423
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-04-27 21:54:13 +0530

    Java programs of gcfl contest

---
+++ src/PoliceRecruit.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PoliceRecruit {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int pm=0;
+        int c=0;
+        int ar=0;
+        for(int i=0;i<n;i++){
+            ar=sc.nextInt();
+            if(ar>0){
+                pm++;
+            }
+            if(ar<=0){
+                if(pm<=0)
+                    c++;
+                else
+                    pm--;
+            }
+        }
+        System.out.println(c);
+    }
+}
