7febaac119777e3ebc3700a0f7927748e10cfd11
Ashish Chaurasia <53967293+Ashish138200@users.noreply.github.com>
2020-04-27 20:06:44 +0530

    Delete ZeroesAndStarPattern.py

--- ZeroesAndStarPattern.py
+++
@@ -1,8 +0,0 @@
-n=int(input())
-for i in range(1,n+1):
-    for j in range(1,2*n+2):
-        if(i==j or j==n+1 or i+j==2*n+2):
-            print("*", end="")
-        else:
-            print("0", end="")
-    print("")
