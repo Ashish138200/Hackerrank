75baecc34d8e42e987057244d0e8b4f3ea2c4f56
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-04-30 15:12:30 +0530

    Java programs of gcfl contest

---
+++ src/BuyAShovel.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BuyAShovel {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int k=sc.nextInt();
+        int r=sc.nextInt();
+        for(int i=1;;i++){
+            if((k*i-r)%10==0 || (k*i)%10==0){
+                System.out.println(i);
+                break;
+            }
+
+        }
+    }
+}
